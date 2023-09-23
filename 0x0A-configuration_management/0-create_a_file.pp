84ed7ebc1787be5d4af00aa90b70fb27bc810381
Uwatt Uko Udeme <uwattuko@gmail.com>
2023-09-23 04:35:56 +0000

    task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,11 @@
+# create a file in /tmp
+
+file { 'tmp':
+    ensure  => file,
+    mode    => '0744',
+    path    => '/tmp/school',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love puppet',
+}
+
