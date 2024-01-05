3b310fa950dd1051a2ededf820e7e4b9348526e8
Uwatt Uko <uwattuko@gmail.com>
2024-01-05 03:02:15 +0000

    task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,36 @@
+#!/usr/python3
+"""dictionary of dictionaries"""
+
+import json
+import requests
+
+
+def export_all_to_json():
+    base_url = "https://jsonplaceholder.typicode.com"
+    users = requests.get(f"{base_url}/users").json()
+
+    all_tasks = {}
+
+    for user in users:
+        user_id = user.get("id")
+        employee = user.get("name")
+
+        tasks = requests.get(f"{base_url}/todos?userId={user_id}").json()
+
+        user_tasks = [
+            {
+                "username": employee,
+                "task": task.get("title"),
+                "completed": task.get("completed")
+            }
+            for task in tasks
+        ]
+
+        all_tasks[user_id] = user_tasks
+
+    with open("todo_all_employees.json", "w") as file:
+        json.dump(all_tasks, file)
+
+
+if __name__ == "__main__":
+    export_all_to_json()
