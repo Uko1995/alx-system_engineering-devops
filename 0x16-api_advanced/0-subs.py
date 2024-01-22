e9599c6707cb3adf7e3b3f226699d98369ad9201
Uko Uwatt <uwattuko@gmail.com>
2024-01-22 06:54:43 +0100

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""queries the Reddit API and returns the number of subscribers"""
+
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    '''function that queries REDDIT API and returns number of subscribers'''
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Uko Uwatt"}
+
+    try:
+        response = get(url, headers=headers, allow_redirects=False)
+
+        if response.status_code == 200:
+            data = response.json()
+            return data["data"]["subscribers"]
+        else:
+            return 0
+    except Exception as e:
+        return e
