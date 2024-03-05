ee0a30305bebbb58b693b0bdf828f3ec85aa70e7
Uko Uwatt <uwattuko@gmail.com>
2024-03-05 08:30:52 +0100

    task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''queries the Reddit API and prints the titles of the first 10
+hot posts listed for a given subreddit'''
+
+from requests import get
+
+
+def top_ten(subreddit):
+    ''' function queries Reddit API and get titles of first 10 hot posts '''
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    header = {"User-Agent": "1-top_ten.py"}
+    param = {"limit": 10}
+    response = get(url, params=param, headers=header, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json().get("data").get("children")
+        for post in data:
+            print(post.get("data").get("title"))
+    else:
+        print("None")
