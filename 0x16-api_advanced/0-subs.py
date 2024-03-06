5ee90c1778d63dc204940cf92a619bd24ff050a6
Uko Uwatt <uwattuko@gmail.com>
2024-03-06 07:37:46 +0100

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,13 +9,10 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Uko Uwatt"}
 
-    try:
-        response = get(url, headers=headers, allow_redirects=False)
+    response = get(url, headers=headers, allow_redirects=False)
 
-        if response.status_code == 200:
-            data = response.json()
-            return data["data"]["subscribers"]
-        else:
-            return 0
-    except Exception as e:
-        return e
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0
