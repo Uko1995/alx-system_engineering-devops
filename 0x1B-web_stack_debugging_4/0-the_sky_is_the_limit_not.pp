266b05c7f17d7802de4c876cfc1eea4ea2c9adb6
Uko Uwatt <uwattuko@gmail.com>
2024-03-15 05:02:08 +0100

    webstack debugging 4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,14 @@
+# Puppet script to fix stack on nginx
+
+# Increasing the ULIMIT of the default file
+file { 'fix-for-nginx':
+  ensure  => 'file',
+  path    => '/etc/default/nginx',
+  content => inline_template('<%= File.read("/etc/default/nginx").gsub(/15/, "4096") %>'),
+}
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => '/etc/init.d/nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Puppet script to change the OS configuration
+
+# Increasing hard file limit for user holberton
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin:/bin',
+}
+
+# Increasing soft file limit for user holberton
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin:/bin',
+}
